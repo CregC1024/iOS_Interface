4693d66f0aebf9a7854df145dd4122a5c8cf18f5
CregC1024 <38873613+CregC1024@users.noreply.github.com>
2022-02-02 23:54:24 -0600

    Add files via upload

---
+++ interface1App.swift
@@ -0,0 +1,17 @@
+//
+//  interface1App.swift
+//  interface1
+//
+//  Created by Creg Sullivan on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct interface1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
